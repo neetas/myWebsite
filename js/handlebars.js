64be632f77b50a52352f245f59989ed7d8ee8b5b
neetas <neeta.sarl@7digital.com>
2016-11-11 21:57:53 +0000

    first example using handle bars

--- js/handlebars.js
+++ js/handlebars.js
@@ -2,8 +2,17 @@ $(document).ready(function () {
   var source = $('#entry-template').html();
   var template = Handlebars.compile(source);
 
-  var context = {title: "My first handle bar",
-                 body: "My first handle bar post!"}
+  var context = {
+    title: "AZURE SHORES",
+    tag0: "Composition",
+    tag1: "Production",
+    tag2: "Mixing",
+    tag3: "Sound Design",
+    subtitle: "Sample Pack for Launchpad app [Novation]",
+    description: "Azure Shores brings a Future Bass inspired soundscape to Launhpad, with a slight tropical twinge. Dive in.",
+    audiolink: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/292485237&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"
+  }
 
   var html = template(context);
+  $(document.body).append(html);
 });
