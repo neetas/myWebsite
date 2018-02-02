ef2dcf95337921db6952ced3f6d33e4d687c205a
neetas <neeta.sarl@7digital.com>
2018-02-02 15:03:05 +0000

    Add contact page

--- js/loadComponents.js
+++ js/loadComponents.js
@@ -1,10 +1,1 @@
-function hideVideos() {
-    $("#vidshowreel").slideUp("slow","linear", function showTabContent(){
-        $(".tab-content").fadeIn("5000");
-    });
-}
 
-function showVideos() {
-    $(".tab-content").hide();
-    $("#vidshowreel").slideDown("slow");
-}
